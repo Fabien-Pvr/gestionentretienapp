7165d6736276c77ed67fd6806b6d04f7bf5bf051
POIVRE Fabien <fpoivre@ensc.fr>
2024-02-13 10:39:46 +0100

    lien sur le logo enlever pour une meilleure navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./CSS/App.css";
 
 // Importez vos composants de page
@@ -7,29 +7,31 @@ import Home from "./Pages/Home";
 import Ajout from "./Pages/Ajout";
 import Historique from "./Pages/Historique";
 import Parametres from "./Pages/Parametres";
+// import Entretien from "./Pages/Entretien"; // Assurez-vous d'avoir un composant pour la page Entretien
 
 import Footer from "./Component_App/Footer";
 import Head from "./Component_App/Head";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Head />{" "}
-        {/* Le composant Head est maintenant présent sur toutes les pages */}
-      </header>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Head />
+        </header>
 
-      <main className="ContainerInit"></main>
+        <main className="ContainerInit"></main>
 
-      <Footer />
+        <Footer />
 
-      <Routes>
-        <Route path="/home/*" element={<Home />} />
-        <Route path="/ajout/*" element={<Ajout />} />
-        <Route path="/historique/*" element={<Historique />} />
-        <Route path="/parametres/*" element={<Parametres />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/home/*" element={<Home />} />
+          <Route path="/ajout/*" element={<Ajout />} />
+          <Route path="/historique/*" element={<Historique />} />
+          <Route path="/parametres/*" element={<Parametres />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
