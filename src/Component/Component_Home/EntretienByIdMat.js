3fd654686ad2deeae7d60571cf24e223723cc7ce
POIVRE Fabien <fpoivre@ensc.fr>
2024-07-17 17:39:05 -0400

    Ajout des entretiens par véhicules avec le Ajout à mettre en fonction

---
+++ src/Component/Component_Home/EntretienByIdMat.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import "../../CSS/Entretien.css";
+import { RecupererModeleMat } from "../Component_queries/queries";
+
+const EntretienByIdMat = ({ entretien }) => {
+  const [selectedEntretienId, setSelectedEntretienId] = useState(null);
+  const [modeleMat, setModeleMat] = useState("");
+
+  useEffect(() => {
+    const fetchModeleMat = async () => {
+      const modele = await RecupererModeleMat(entretien.IdMat);
+      setModeleMat(modele);
+    };
+
+    fetchModeleMat();
+  }, [entretien.IdMat]);
+
+  const toggleEntretien = (id) => {
+    setSelectedEntretienId((prevId) => (prevId === id ? null : id));
+  };
+
+  return (
+    <div>
+      <ul className="ContainerAll_Entretien">
+        <div
+          className="Entretien-ContainerFrame-Fille"
+          key={entretien.IdEntretien}
+          onClick={() => toggleEntretien(entretien.IdEntretien)}
+        >
+          {/* <p className="ptest1">{modeleMat}</p> */}
+          <p className="ptest2">{entretien.TypeEntretien}</p>
+          <p className="ptest4">{entretien.Date}</p>
+          <p className="ptest3">{entretien.NbHeure} heures au compteur</p>
+          {selectedEntretienId === entretien.IdEntretien && (
+            <p className="ptest3">{entretien.Observation}</p>
+          )}
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default EntretienByIdMat;
--- src/Component/Component_Home/VehicleDetails.js
+++ src/Component/Component_Home/VehicleDetails.js
@@ -1,23 +1,51 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import "../../CSS/MaterielList.css";
 import useGetMaterielData from "./UseGetMaterielData.js";
 import MaterielByID from "./MaterielID.js";
 import BesoinEntretienID from "./BesoinEntretienByIDMat.js";
-import { useNavigate } from "react-router-dom";
 import useGetNoticeByIdMat from "../Component_queries/useNoticeDataByIdMat.js";
+import useGetEntretienByIdMat from "../Component_queries/useGetEntretienByIdMat.js";
+import EntretienByIdMat from "./EntretienByIdMat.js";
+
+const ChoixNoticesEntretiens = ({ activeBox, handleClick }) => {
+  return (
+    <div className="barreChoix">
+      <div
+        className={activeBox === "Notices" ? "nomActive" : "nomUnactive"}
+        onClick={() => handleClick("Notices")}
+      >
+        Notices
+      </div>
+      <div
+        className={activeBox === "Entretiens" ? "nomActive" : "nomUnactive"}
+        onClick={() => handleClick("Entretiens")}
+      >
+        Entretiens
+      </div>
+    </div>
+  );
+};
 
 const VehicleDetails = () => {
   const { vehicleId } = useParams();
   const valueMateriel = useGetMaterielData({ vehicleId });
   const notices = useGetNoticeByIdMat(vehicleId) || [];
+  const entretiens = useGetEntretienByIdMat(vehicleId) || [];
+
   const materielInfo = valueMateriel.materielInfo;
+
+  const [activeBox, setActiveBox] = useState("Notices");
 
   const navigate = useNavigate();
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     return date.toLocaleDateString("fr-FR");
+  };
+
+  const handleClick = (page) => {
+    setActiveBox(page);
   };
 
   const HandleClickNotice = (IdBesoinEntretien) => {
@@ -40,8 +68,15 @@ const VehicleDetails = () => {
           </>
         )}
       </div>
-      <div className="Titre">Notices disponible</div>
-      {notices &&
+      <div className="Titre">
+        <ChoixNoticesEntretiens
+          activeBox={activeBox}
+          handleClick={handleClick}
+        />
+        <div className="Container_Ajouter_detailVehicule_Ajouter">
+          <button className="button_Ajouter_detailVehicule">+</button></div>
+      </div>
+      {activeBox === "Notices" &&
         notices.map(
           (item) =>
             item && (
@@ -50,6 +85,17 @@ const VehicleDetails = () => {
                 onClick={() => HandleClickNotice(item.IdBesoinEntretien)}
               >
                 <BesoinEntretienID notice={item} />
+              </div>
+            )
+        )}
+      {activeBox === "Entretiens" &&
+        entretiens.map(
+          (item) =>
+            item && (
+              <div
+                key={item.idEntretien}
+              >
+                <EntretienByIdMat entretien={item} />
               </div>
             )
         )}
