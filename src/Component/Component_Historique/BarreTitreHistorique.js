40aa4335542f8c2922f70f8df925b422ef383d8c
POIVRE Fabien <fpoivre@ensc.fr>
2024-07-18 13:19:47 -0400

    mise en place de Ajout sur les autres pages

--- src/Component/Component_Historique/BarreTitreHistorique.js
+++ src/Component/Component_Historique/BarreTitreHistorique.js
@@ -2,14 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../CSS/ChoixMaterielNouveau.css";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const BarreTitreHistorique = () => {
   const [activePage, setActivePage] = useState("historique");
+  const navigate = useNavigate();
 
   const handleClick = (page) => {
     setActivePage(page);
   };
 
+  const HandleClickAjout = (activeBox) => {
+    navigate(`/ajout/${activeBox}`, { state: { activeBox } });
+  };
   return (
     <div className="barreChoix">
       <Link
@@ -19,6 +24,9 @@ const BarreTitreHistorique = () => {
       >
         Historique
       </Link>
+      <p onClick={() => HandleClickAjout("entretien")} className="nomUnactive">
+        Ajouter +
+      </p>
     </div>
   );
 };
